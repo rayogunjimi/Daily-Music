b3535411794ab903923752a04d9b2040aa5d9ee7
Ray Ogunjimi <raogunjimi@gmail.com>
2021-11-19 15:21:53 -0500

    Add additional genres

--- dailymusic.py
+++ dailymusic.py
@@ -2,21 +2,29 @@ def randomizer():
     import random
     import json
 
-    #TODO: Create genres using Spotify API
-    #TODO: Add catch for no file found
-    handle_genres = open("genres.json", "r")
-    dict_genres = json.load(handle_genres)
-    index_genres = random.randrange(0, len(dict_genres["genres"]))
-    current_genre = dict_genres["genres"][index_genres]
-    print("Today's genre: ", current_genre["name"])
-    handle_artist = open(current_genre["path"], "r")
-
-    # TODO: Add catch for empty artist list
-    list_artist = handle_artist.read().splitlines()
-    index_aritst = random.randrange(0, len(list_artist))
-
-    print("Today's artist is: ", list_artist[index_aritst])
-    #TODO: Add today's album using Spotify API
+    try:
+        #TODO: Create genres using Spotify API
+        handle_genres = open("genres.json", "r")
+        dict_genres = json.load(handle_genres)
+        try:
+            index_genres = random.randrange(0, len(dict_genres["genres"]))
+            current_genre = dict_genres["genres"][index_genres]
+            print("Today's genre: ", current_genre["name"])
+            try:
+                handle_artist = open(current_genre["path"], "r")
+                try:
+                    list_artist = handle_artist.read().splitlines()
+                    index_aritst = random.randrange(0, len(list_artist))
+                    print("Today's artist is: ", list_artist[index_aritst])
+                    #TODO: Add today's album using Spotify API
+                except:
+                    print("Your genre has no artists.")
+            except:
+                print("Artist file not found for the selected genre.")
+        except:
+            print("There was an issue reading your genre.")
+    except:
+        print("genres.json file not found.")
 
 def main():
     randomizer()
